53823c2359c3ce040d495988eaad08ddf64d09a2
Our project <kn@brokenfruit.dk>
2016-04-23 01:11:41 +0200

    Bug fixes and sanitaztion
    - Added fix for “Missing argument 2 for do_settings_fields()”
    - Splittet the admin.php file
    - Reordered code

---
+++ Custom Login Shortcode/custom-login-shortcode/admin/admin-page.php
@@ -0,0 +1,98 @@
+<?php 
+
+// Buidling the HTML page
+
+if(!current_user_can('manage_options'))
+    {
+        wp_die( __('You do not have sufficient permissions to access this page.','clsc') );
+    }
+
+    ?>
+
+    <script type="text/javascript">
+
+    	$j = jQuery.noConflict();
+
+    	var default_logout = <?php echo json_encode( wp_logout_url( home_url()) ); ?>;
+
+    	$j(document).ready(function(){
+		    $j("#logout-default").click(function(){
+		        $j("#logout-field").val(default_logout);
+		    });
+		});
+
+    </script>
+
+    <div class="wrap">
+
+        <form method="post" action="options.php"> 
+            <?php 
+
+            $options = get_option('clsc_options_array');
+
+            settings_fields('clsc-settings-group');
+            do_settings_fields('clsc-settings-group'); 
+
+            ?>          
+            <div class="bootstrap-wrapper">
+	            <div class="row">
+	                <div class="col-md-12">
+	                    <h1><?php _e('Custom Login Shortcode','clsc'); ?></h1>
+	                    <p><?php _e('To use for shortcode:','clsc'); ?><br/><span class="shortcode-preview">[custom_login]</span></p>
+	                </div>
+	            </div>
+	            <div class="row" id="login-content">
+	                <div class="col-md-4">
+	                    <h5><?php _e('Log in link:','clsc'); ?></h5>
+	                    <input name="clsc_options_array[Login_link]" placeholder="<?php _e('Example: log-in/', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Login_link']; ?>" />
+	                </div>
+	                <div class="col-md-4">
+	                    <h5><?php _e('Log in string:','clsc'); ?></h5>
+	                    <input name="clsc_options_array[Login_string]" placeholder="<?php _e('Example: Log in', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Login_string']; ?>" />
+	                </div>
+	                <div class="col-md-4">
+	                    <h5><?php _e('Log in class:','clsc'); ?></h5>
+	                    <input name="clsc_options_array[Login_class]"  placeholder="<?php _e('Example: login_style', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Login_class']; ?>" />
+	                </div>
+	            </div>
+	            <div class="row top-buffer" id="logout-content">
+	                <div class="col-md-4">
+	                    <h5><?php _e('Log out link:','clsc'); ?></h5>
+	                    <input id="logout-field" name="clsc_options_array[Logout_link]" placeholder="<?php _e('Example: log-out/', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Logout_link']; ?>" />
+	                	<input class="btn btn-default btn-xs" type="button" name="logout-default" id="logout-default" value="<?php _e('Use default logout link','clsc') ?>"/>
+	                </div>
+	                <div class="col-md-4">
+	                    <h5><?php _e('Log out string:','clsc'); ?></h5>
+	                    <input name="clsc_options_array[Logout_string]" placeholder="<?php _e('Example: Log out', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Logout_string']; ?>" />
+	                </div>
+	                <div class="col-md-4">
+	                    <h5><?php _e('Log out class:','clsc'); ?></h5>
+	                    <input name="clsc_options_array[Logout_class]" placeholder="<?php _e('Example: logout_style', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Logout_class']; ?>" />
+	                </div>
+	            </div>
+	            <div class="row top-buffer" id="account-content">
+	                <div class="col-md-4">
+	                    <h5><?php _e('Account link:','clsc'); ?></h5>
+	                    <input name="clsc_options_array[Account_link]" placeholder="<?php _e('Example: my-account/', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Account_link']; ?>" />
+	                </div>
+	                <div class="col-md-4">
+	                    <h5><?php _e('Account string:','clsc'); ?></h5>
+	                    <input name="clsc_options_array[Account_string]" placeholder="<?php _e('Example: My Account', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Account_string']; ?>" />
+	                </div>
+	                <div class="col-md-4">
+	                    <h5><?php _e('Account class:','clsc'); ?></h5>
+	                    <input name="clsc_options_array[Account_class]" placeholder="<?php _e('Example: account_style', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Account_class']; ?>" />
+	                </div>
+	            </div>
+	            <div class="row top-buffer" id="submit">
+	                <div class="col-md-4">
+	                    <input class="btn btn-primary" type="submit" name="submit" id="submit" value="<?php _e('Save Changes', 'clsc') ?>" />
+	                </div>
+	            </div> 
+            </div>
+        </form>
+    </div>
+
+    <?php
+
+?>
--- Custom Login Shortcode/custom-login-shortcode/admin/admin.php
+++ Custom Login Shortcode/custom-login-shortcode/admin/admin.php
@@ -24,25 +24,44 @@ function clsc_install() {
 	add_option('clsc_options_array', $clsc_options, '', 'yes');
 }
 
-// Register settings for wordpress to handle all values
-function admin_init_register_setting()
-{    
-    register_setting('wp_plugin_template-group', 'clsc_options_array');
 
-}
-add_action('admin_init','admin_init_register_setting');
+function add_clsc_option_admin_page() {
 
-
-// Create admin option page
-function add_clsc_option_page() {
+	// Create custom admin option page
 	add_options_page(
 		'Custom Login', 			// The text to be displayed in the title tag
 		'Custom Login', 			// The text to be used for the menu
 		'administrator', 			// The capability required to display this menu
 		'custom-login-shortcodes', 	// The unique slug name to refer to this menu
 		'clsc_html_page');			// The function to output the page content
+
+	// Activate custom settings
+	add_action('admin_init','clsc_custom_setting');
 }
-add_action('admin_menu', 'add_clsc_option_page');
+add_action('admin_menu', 'add_clsc_option_admin_page');
+
+// Register settings for wordpress to handle all values
+function clsc_custom_setting() {  
+
+    register_setting('clsc-settings-group', 'clsc_options_array', 'clsc_sanitize_callback');
+
+}
+
+function clsc_sanitize_callback( $input ) {
+
+	// Initialize the new array that will hold the sanitize values
+	$new_input = array();
+
+	// Loop through the input and sanitize each of the values
+	foreach ($input as $key => $val) {
+
+		$new_input[ $key ] = sanitize_text_field( $val );
+
+	}
+
+	return $new_input;
+
+}
 
 // Enqueue admin scripts
 function clsc_enqueue_scripts() {
@@ -59,97 +78,8 @@ add_action('admin_enqueue_scripts', 'clsc_enqueue_scripts' );
 
 function clsc_html_page() {
 
-    if(!current_user_can('manage_options'))
-    {
-        wp_die( __('You do not have sufficient permissions to access this page.','clsc') );
-    }
+	require_once('admin-page.php');
 
-    ?>
-
-    <script type="text/javascript">
-
-    	$j = jQuery.noConflict();
-
-    	var default_logout = <?php echo json_encode( wp_logout_url( home_url()) ); ?>;
-
-    	$j(document).ready(function(){
-		    $j("#logout-default").click(function(){
-		        $j("#logout-field").val(default_logout);
-		    });
-		});
-
-    </script>
-
-    <div class="wrap">
-
-        <form method="post" action="options.php"> 
-            <?php 
-
-            $options = get_option('clsc_options_array');     
-            settings_fields('wp_plugin_template-group');
-            do_settings_fields('wp_plugin_template-group'); 
-
-            ?>          
-            <div class="bootstrap-wrapper">
-	            <div class="row">
-	                <div class="col-md-12">
-	                    <h1><?php _e('Custom Login Shortcode','clsc'); ?></h1>
-	                    <p><?php _e('To use for shortcode:','clsc'); ?><br/><span class="shortcode-preview">[custom_login]</span></p>
-	                </div>
-	            </div>
-	            <div class="row" id="login-content">
-	                <div class="col-md-4">
-	                    <h5><?php _e('Log in link:','clsc'); ?></h5>
-	                    <input name="clsc_options_array[Login_link]" placeholder="<?php _e('Example: log-in/', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Login_link']; ?>" />
-	                </div>
-	                <div class="col-md-4">
-	                    <h5><?php _e('Log in string:','clsc'); ?></h5>
-	                    <input name="clsc_options_array[Login_string]" placeholder="<?php _e('Example: Log in', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Login_string']; ?>" />
-	                </div>
-	                <div class="col-md-4">
-	                    <h5><?php _e('Log in class:','clsc'); ?></h5>
-	                    <input name="clsc_options_array[Login_class]"  placeholder="<?php _e('Example: login_style', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Login_class']; ?>" />
-	                </div>
-	            </div>
-	            <div class="row top-buffer" id="logout-content">
-	                <div class="col-md-4">
-	                    <h5><?php _e('Log out link:','clsc'); ?></h5>
-	                    <input id="logout-field" name="clsc_options_array[Logout_link]" placeholder="<?php _e('Example: log-out/', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Logout_link']; ?>" />
-	                	<input class="btn btn-default btn-xs" type="button" name="logout-default" id="logout-default" value="<?php _e('Use default logout link','clsc') ?>"/>
-	                </div>
-	                <div class="col-md-4">
-	                    <h5><?php _e('Log out string:','clsc'); ?></h5>
-	                    <input name="clsc_options_array[Logout_string]" placeholder="<?php _e('Example: Log out', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Logout_string']; ?>" />
-	                </div>
-	                <div class="col-md-4">
-	                    <h5><?php _e('Log out class:','clsc'); ?></h5>
-	                    <input name="clsc_options_array[Logout_class]" placeholder="<?php _e('Example: logout_style', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Logout_class']; ?>" />
-	                </div>
-	            </div>
-	            <div class="row top-buffer" id="account-content">
-	                <div class="col-md-4">
-	                    <h5><?php _e('Account link:','clsc'); ?></h5>
-	                    <input name="clsc_options_array[Account_link]" placeholder="<?php _e('Example: my-account/', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Account_link']; ?>" />
-	                </div>
-	                <div class="col-md-4">
-	                    <h5><?php _e('Account string:','clsc'); ?></h5>
-	                    <input name="clsc_options_array[Account_string]" placeholder="<?php _e('Example: My Account', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Account_string']; ?>" />
-	                </div>
-	                <div class="col-md-4">
-	                    <h5><?php _e('Account class:','clsc'); ?></h5>
-	                    <input name="clsc_options_array[Account_class]" placeholder="<?php _e('Example: account_style', 'clsc') ?>" class="form-control" type="text" value="<?php echo $options['Account_class']; ?>" />
-	                </div>
-	            </div>
-	            <div class="row top-buffer" id="submit">
-	                <div class="col-md-4">
-	                    <input class="btn btn-primary" type="submit" name="submit" id="submit" value="<?php _e('Save Changes', 'clsc') ?>" />
-	                </div>
-	            </div> 
-            </div>
-        </form>
-    </div>
-
-    <?php
 }
 
 ?>
